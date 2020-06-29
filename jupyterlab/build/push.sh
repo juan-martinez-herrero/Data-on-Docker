405fa23b2d7c75004aea00cba5460b40f5e85aaf
git <juan.martinez@veinteractive.com>
2020-06-30 01:14:23 +0200

    add all changes done in pandemia time

---
+++ jupyterlab/build/push.sh
@@ -0,0 +1,18 @@
+set -ex
+
+# docker hub username
+USERNAME=juanmartinez
+# image name
+IMAGE=jupyterlab
+
+version=`cat VERSION`
+buildDate=`date +"%y.%m.%d"`
+
+# login dockerhub
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# push it
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version
+docker push $USERNAME/$IMAGE:$version-$buildDate
